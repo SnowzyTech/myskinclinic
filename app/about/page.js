0e0d402c4ac3c3501bbfb57ffe487d7c68e29b5d
snowzyTech <ihekunaemmanuelc@gmail.com>
2025-09-10 21:37:36 +0100

    Updated

--- app/about/page.js
+++ app/about/page.js
@@ -265,7 +265,7 @@ const AboutPage = () => {
             </div>
           </ScrollAnimation>
 
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
             {[
               {
                 name: "Mrs Chisom Geraldine",
